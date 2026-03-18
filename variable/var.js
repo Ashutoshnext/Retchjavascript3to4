0870b01616a50224b6f7f72dda51fc5fc50fa578
Ashutosh Kumar <ashutoshpal911097@gmail.com>
2026-03-18 16:01:03 +0530

    'variable'

---
+++ variable/var.js
@@ -0,0 +1,35 @@
+// var ax='hello world' 
+// var a;
+// a='mohna'
+// var a='raj'
+// a='rohan'
+
+// console.log(a);
+
+
+// let a='ram';
+// a='rohan';
+// let a='mohan';
+
+// console.log(a);
+
+
+// const a='raj'
+// // a='mohan'
+// // const a;
+// console.log(a);
+
+
+
+// scope 
+
+const a='ram'
+{
+    console.log(a);
+}
+
+
+
+
+
+
